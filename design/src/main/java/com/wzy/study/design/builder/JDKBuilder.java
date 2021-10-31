183df0661ffa8bd9a41e2980512cee244703753f
wangzongyi <wangongyichina@163.com>
2021-10-31 17:06:00 +0800

    设计模式

---
+++ design/src/main/java/com/wzy/study/design/builder/JDKBuilder.java
@@ -0,0 +1,22 @@
+package com.wzy.study.design.builder;
+
+/**
+ * @Author: wangzongyi
+ * @Data: 2021/10/30 15:20
+ * @Desc:
+ */
+
+public class JDKBuilder {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("hello Builder");
+        System.out.println(builder);
+
+        /**
+         * Appendable 接口定义多个 append 方法。即 Appendable 为抽象建造者，定义了抽象方法
+         * AbstractStringBuilder 虽然是抽象方法，但其实已经实现了 appendable 接口的方法，其实塔已经是
+         *     建造者了，只是不能实例化而已
+         * StringBuilder 即充当了指挥者的角色，同时充当了具体建造者的角色，建造方法的实现是由 AbstractStringBuilder
+         *     已经实现了，StringBuilder 是继承了 父类的方法。
+         */
+    }
+}
---
+++ design/src/main/java/com/wzy/study/design/factory/JDKFactory.java
@@ -0,0 +1,16 @@
+package com.wzy.study.design.factory;
+
+import java.util.Calendar;
+
+/**
+ * @Author: wangzongyi
+ * @Data: 2021/10/26 20:56
+ * @Desc:
+ */
+
+public class JDKFactory {
+    public static void main(String[] args) {
+        Calendar.getInstance();
+
+    }
+}
